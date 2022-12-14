5981824ad55e29f648454e002ef4c58e00eefdf0
Alex <DiaA6383@gmail.com>
2022-12-14 12:07:46 -0500

    added mem files

---
+++ Memory.cpp
@@ -0,0 +1,40 @@
+#include "Memory.h"
+#include <iostream>
+
+Memory::Memory(int m_pageNum_, int m_pid_f, int m_priority_f){
+    setMemPageNum(m_pageNum_);
+    setMemPID(m_pid_f);
+    setMemPriority(m_priority_f);
+
+    use_ = 0;
+}
+
+int Memory::getMemPageNum()const{
+    return pageNum_;
+}
+int Memory::getMemPID()const{
+    return pid_f;
+}
+int Memory::getMemUse()const{
+    return use_;
+}
+int Memory::getMemPriority()const{
+    return priority_f;
+}
+
+
+void Memory::setMemPageNum(int m_pageNum){
+    pageNum_ = m_pageNum;
+}
+void Memory::setMemPID(int m_pid_f){
+    pid_f = m_pid_f;
+}
+void Memory::incrementUse(){
+    use_++;
+}
+void Memory::setMemPriority(int m_priority){
+    priority_f = m_priority;
+
+}
+
+
---
+++ Memory.h
@@ -0,0 +1,32 @@
+#ifndef MEMORY_
+#define MEMORY_
+#include <iostream>
+#include <vector>
+class Memory{
+    public:
+    Memory(int m_pageNum, int m_pid_f,int m_priority_f);
+    int getMemPageNum()const;
+    int getMemPID()const;
+    int getMemUse()const;
+    int getMemPriority()const;
+
+
+    void setMemPageNum(int m_pageNum);
+    void setMemPriority(int m_priority);
+    void setMemPID(int m_pid_f);
+    void incrementUse();
+
+    
+
+
+    private:
+    int pageNum_;
+    int pid_f;
+    int use_;
+    int priority_f;
+   
+    
+
+};
+
+#endif
